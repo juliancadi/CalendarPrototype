b5083515d3edfdb27d4cd6b08ff8b107d72107e6
Julian Caicedo <me@juliancadi.com>
2018-09-16 12:09:44 +0200

    Install FSCalendar pod

---
+++ CalendarPrototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalendarPrototype
+//
+//  Created by Julian Caicedo on 16.09.18.
+//  Copyright © 2018 juliancadi.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-CalendarPrototype/Pods-CalendarPrototype-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CalendarPrototype : NSObject
+@end
+@implementation PodsDummy_Pods_CalendarPrototype
+@end
